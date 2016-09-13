cafaafebe02092165d2a876c6d7dd310b818bada
ohad-denh <ohaddenhorst@OliverHorstsAir.localdomain>
2016-09-13 17:01:22 +0200

    good bye change
    text geändert

---
+++ src/Providers/HelloWorldServiceProvider.hh
@@ -0,0 +1,14 @@
+<?hh //strict
+
+    namespace HelloWorld\Providers;
+
+
+    use Plenty\Plugin\ServiceProvider;
+
+    class HelloWorldServiceProvider extends ServiceProvider
+    {
+        public function register():void
+        {
+          $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+        }
+    }
